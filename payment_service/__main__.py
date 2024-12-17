7b0563c4268bf8d0a429e94d72f1cf13ab80b357
Daniel Fortich <fortichdaniel16@gmail.com>
2024-12-17 17:46:38 -0500

    strategy pattern

--- payment_service/__main__.py
+++ payment_service/__main__.py
@@ -2,7 +2,7 @@ import os
 
 from commons import CustomerData, ContactInfo
 
-from notifiers import EmailNotifier
+from notifiers import NotifierProtocol, EmailNotifier, SMSNotifier
 
 from service import PaymentService
 
@@ -11,6 +11,23 @@ from processors import StripePaymentProcessor
 from loggers import TransactionLogger
 
 from validators import CustomerValidator, PaymentDataValidator
+
+
+def get_email_notifier() -> EmailNotifier:
+    return EmailNotifier()
+
+
+def get_sms_notifier() -> SMSNotifier:
+    return SMSNotifier(gateway="SMSGatewayExample")
+
+
+def get_notifier_implementation(customer_data: CustomerData) -> NotifierProtocol:
+    if customer_data.contact_info.phone:
+        return get_sms_notifier()
+    elif customer_data.contact_info.email:
+        return get_email_notifier()
+    else:
+        ValueError("Cannot choice the correct implementation")
 
 
 def get_customer_data() -> CustomerData:
@@ -27,7 +44,9 @@ stripe_payment_processor = StripePaymentProcessor(
 
 customer_data = get_customer_data()
 
-notifier = EmailNotifier()
+notifier = get_notifier_implementation(customer_data)
+email_notifier = get_email_notifier()
+sms_notifier = get_sms_notifier()
 
 customer_validator = CustomerValidator()
 
@@ -42,3 +61,9 @@ service = PaymentService(
     payment_validator=payment_validator,
     logger=logger,
 )
+
+# Change notification strategy to email
+service.set_notifier(email_notifier)
+
+# Change notification strategy to sms
+service.set_notifier(sms_notifier)
